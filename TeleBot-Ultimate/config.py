ba9545ed8c2e3cb57df1d4a318fec6a32e407c3f
atotdf-create <user@example.com>
2026-06-22 23:50:10 +0000

    Hardcode bot token in config.py

--- TeleBot-Ultimate/config.py
+++ TeleBot-Ultimate/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8021833923:AAGDUT7aH9r_j3VY5__BoAlaVg_lpey1kO0"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
